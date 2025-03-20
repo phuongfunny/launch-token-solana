72eea800f7074e81b9d5bb1905142ec63526a78b
an.hoang <an.hoang@lunardigital.net>
2025-03-20 21:57:45 +0700

    fix tailwind css

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,20 @@
-import type { Config } from "tailwindcss";
+// import type { Config } from "tailwindcss";
 
-const config: Config = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+// const config: Config = {
+//   content: ["./src/**/*.{js,jsx,ts,tsx}"],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// };
+
+// export default config;
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
 };
-
-export default config;
